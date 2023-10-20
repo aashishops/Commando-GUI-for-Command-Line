b876aa7084b35b43b3d168f19594fc25586d6de3
aashishops <107553285+aashishops@users.noreply.github.com>
2023-10-20 13:23:20 +0530

    added git commands

--- Commando.py
+++ Commando.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog  # Import the necessary module for the commit message dialog
 import subprocess
 import os
 
@@ -8,6 +8,9 @@ command_history = []
 
 # Variable to store the selected folder path
 selected_folder_path = ""
+
+# Declare current_directory_label as a global variable
+current_directory_label = None
 
 # Function to execute the command and display the output
 def execute_command():
@@ -52,69 +55,29 @@ def browse_folder():
         except OSError as e:
             current_directory_label.config(text="Error: " + str(e))
 
-# Function to create a new instance of the Command Line GUI
-def create_new_gui():
-    new_root = tk.Tk()
-    new_root.title("Command Line GUI")
+# Function to execute Git commands
+def execute_git_command(command):
+    try:
+        result = subprocess.check_output(command, shell=True, text=True, stderr=subprocess.STDOUT)
 
-    # Dark mode colors
-    bg_color = "#2E2E2E"  # Dark gray background
-    fg_color = "#FFFFFF"  # White text
-    entry_bg_color = "#3E3E3E"  # Slightly lighter background for entry widget
-    button_bg_color = "#4E4E4E"  # Slightly lighter background for button widget
-    title_color = "#FFA500"  # Orange title color
+        # Append the executed Git command to the history
+        command_history.append(command)
+        update_command_history()
 
-    # Set window background color
-    new_root.configure(bg=bg_color)
+        # Clear the input field
+        command_entry.delete(0, tk.END)
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the Git command execution
+        result = f"Error: {e.output}"
 
-    # Set title color
-    title_bar = tk.Label(new_root, text="Commando", bg=bg_color, fg=title_color)
-    title_bar.pack()
+    # Display the result in the console
+    print(result)
 
-    # Create a menu bar
-    menu_bar = tk.Menu(new_root)
-    new_root.config(menu=menu_bar)
-
-    # Add menu items
-    file_menu = tk.Menu(menu_bar, tearoff=0)
-    menu_bar.add_cascade(label="File", menu=file_menu)
-    file_menu.add_command(label="New", command=create_new_gui)
-    file_menu.add_command(label="Open")
-    file_menu.add_command(label="Delete")
-    menu_bar.add_command(label="Git")
-    menu_bar.add_command(label="Run")
-
-    # Label to display the selected folder path
-    folder_path_label = tk.Label(new_root, text="Selected Folder Path: " + selected_folder_path, bg=bg_color, fg=fg_color)
-    folder_path_label.pack()
-
-    # Button to browse and select a folder
-    browse_folder_button = tk.Button(new_root, text="Browse Folder", command=browse_folder, bg=button_bg_color, fg=fg_color)
-    browse_folder_button.pack()
-
-    # Entry widget to input the command
-    command_label = tk.Label(new_root, text="Enter a command:", bg=bg_color, fg=fg_color)
-    command_label.pack()
-    command_entry = tk.Entry(new_root, width=40, bg=entry_bg_color, fg=fg_color)
-    command_entry.pack(pady=5)
-
-    # Bind the Enter key to execute the command
-    command_entry.bind("<Return>", on_enter)
-
-    # Button to execute the command
-    execute_button = tk.Button(new_root, text="Execute", command=execute_command, bg=button_bg_color, fg=fg_color)
-    execute_button.pack()
-
-    # Command history text widget
-    history_label = tk.Label(new_root, text="Command History:", bg=bg_color, fg=fg_color)
-    history_label.pack()
-    history_text = tk.Text(new_root, height=10, width=50, state=tk.DISABLED, bg=bg_color, fg=fg_color)
-    history_text.pack()
-
-    # Initialize the command history
-    update_command_history()
-
-    new_root.mainloop()
+# Function to commit changes with a custom message
+def commit_changes():
+    commit_message = simpledialog.askstring("Commit Message", "Enter your commit message:")
+    if commit_message is not None:
+        execute_git_command(f'git commit -m "{commit_message}"')
 
 # Create the main GUI window
 root = tk.Tk()
@@ -141,10 +104,16 @@ root.config(menu=menu_bar)
 # Add menu items
 file_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="File", menu=file_menu)
-file_menu.add_command(label="New", command=create_new_gui)
+file_menu.add_command(label="New")
 file_menu.add_command(label="Open")
 file_menu.add_command(label="Delete")   
-menu_bar.add_command(label="Git")
+git_menu = tk.Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Git", menu=git_menu)
+git_menu.add_command(label="Initialize Git Repository", command=lambda: execute_git_command("git init"))
+git_menu.add_command(label="Add Files to Staging", command=lambda: execute_git_command("git add ."))
+git_menu.add_command(label="Commit Changes", command=commit_changes)
+git_menu.add_command(label="Push to Remote", command=lambda: execute_git_command("git push -u origin main"))
+git_menu.add_command(label="Pull from Remote", command=lambda: execute_git_command("git pull"))
 run_menu = tk.Menu(menu_bar, tearoff=0)
 menu_bar.add_cascade(label="Run", menu=run_menu)
 run_menu.add_command(label="Python")
